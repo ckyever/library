26b3f18ba63f4fa5023ff96d2bc463a6d4211e50
ckyever <ckyeverg@gmail.com>
2025-09-25 17:53:34 +1000

    Add ability to add books to a library

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+  let newBook = Book(title, author, pages);
+  myLibrary.push(newBook);
+}
